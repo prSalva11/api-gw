5f277c19c3e17b7108a0133c6bef02e9218b0098
Prasenjit Salva <prasenjitsalva11@gmail.com>
2024-02-24 17:50:19 +0530

    adding initial branch

---
+++ src/test/java/com/yourvoice/lb/apigw/ApiGwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourvoice.lb.apigw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
